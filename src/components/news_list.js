ccfbbb5dfd19aba93ce4bc7ca74b6b1247ab841d
jpitman1010 <jpitman1010@gmail.com>
2021-02-01 13:12:03 -0800

    props part 2

--- src/components/news_list.js
+++ src/components/news_list.js
@@ -1,27 +1,48 @@
+// import React from 'react';
+
+// const NewsList = (props) => {
+    
+//     console.log(props)
+
+//     const news =    props.news.map((item) =>(
+//         <div key={item.id}>
+//             <h3>{item.title}</h3>
+//            <div >
+//                {item.feed}
+//            //map is a fcn within React, allows you to loop through the dict object
+//            </div>
+//         </div>
+//     )
+
+//     )
+
+//     return (
+//         <>
+//         { news }
+//         </>
+//     )
+// }
+
+
+// export default NewsList;
+
+
 import React from 'react';
+import NewsItem from './news_list_item';
 
 const NewsList = (props) => {
     
     console.log(props)
 
-    const news =    props.news.map((item) =>(
-        <div key={item.id}>
-            <h3>{item.title}</h3>
-           <div >
-               {item.feed}
-           //map is a fcn within React, allows you to loop through the dict object
-           </div>
-        </div>
-    )
-
-    )
-
+    const news = props.news.map((item) =>(
+        <NewsItem item={item} key={item.id}/>
+                       
+    ))
     return (
         <>
         { news }
         </>
     )
 }
-
 
 export default NewsList;
