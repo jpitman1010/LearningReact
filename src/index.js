848077a07199732e6d165f9b39502fa66266fc63
jpitman1010 <jpitman1010@gmail.com>
2021-02-01 13:29:25 -0800

    PROPS passed from a class component to a class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React, { createElement } from 'react';
+// import React, { createElement } from 'react';
+
+//starting with PROPS lession: add component into import instad of createElement
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 // to start app in browser, get into src folder for myapp and then type npm start into terminal
 import Header from './components/header';
 //after creating header.js can now import it as long as export statement in bottom of that file
 //  import './styles/style.css'; if you wish to import from within components folder, but I preferred the import via link on html
 
-const App = () => (
+
+import JSON from './styles/db.json';
+//importing the json file to use the data
+import NewsList from './components/news_list';
+//importing the NewsList, also have to add tag below like we did with header
+
+import Footer from './components/footer';
+
+
+// const App = () => (
     // return(createElement('h1',{className:'title'}, React.createElement('div'))
     // return(
     //     <div>
@@ -43,14 +55,32 @@ const App = () => (
 
 
         //once you  create and import header.js, you can now use this in the return statement of index
-        <>
-            <Header/>
-        </>
- 
-        
+class App extends Component {
 
-   
-)
+    state = {
+        news: JSON,
+        footerText: 'The footer has successfully been added to the state',
+    }
+
+    render(){
+        console.log(this.state.news)
+        //this will show you that you have indeed imported JSON file like you wanted.
+        
+        const {news, footerText} = this.state;
+        //by creating this variable, you can not have to type this.state each time
+        
+        return(
+            <>
+                <Header/>
+                <NewsList 
+                    news={news}
+                />
+                <Footer footerText={footerText}/>
+            </>
+            )
+        }
+}
+    
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 //grabs whatever logic expressed as an app fuction (above) and put 
