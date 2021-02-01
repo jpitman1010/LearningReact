fc65c1eadd3e379f231f5cbee412f3793150d047
jpitman1010 <jpitman1010@gmail.com>
2021-02-01 12:39:19 -0800

    completed PROPS lesson 1

---
+++ src/components/news_list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const NewsList = (props) => {
+    
+    console.log(props)
+
+    const news =    props.news.map((item) =>(
+        <div key={item.id}>
+            <h3>{item.title}</h3>
+           <div >
+               {item.feed}
+           //map is a fcn within React, allows you to loop through the dict object
+           </div>
+        </div>
+    )
+
+    )
+
+    return (
+        <>
+        { news }
+        </>
+    )
+}
+
+
+export default NewsList;
